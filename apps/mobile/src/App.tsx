4216c14c456ed04b05f3e330c5c5bfca59f42573
JeanJuba <jeanwsj96@hotmail.com>
2024-07-27 01:03:13 -0300

    Finish most of web frontend application

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -4,7 +4,7 @@ import { somar } from '@barba/core';
 export default function App() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>App {somar(32, 48)}</Text>
+            <Text>Mobile</Text>
         </View>
     )
 }
